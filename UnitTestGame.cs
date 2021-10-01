52547d56ec36c795082c0ee177175c0aceec529d
Jan Suhrmann <s.jan@web.de>
2021-10-01 11:53:46 +0200

    Fix test matchdays

--- UnitTestGame.cs
+++ UnitTestGame.cs
@@ -1954,7 +1954,7 @@ namespace CornerkickUnitTest
       const bool bTestIO = false;
       const int iLand = 36;
 
-      CornerkickManager.Main mn = new CornerkickManager.Main(bContinuingTime: true, iTrainingsPerDay: 2, iTrainingsPerDayMax: 3);
+      CornerkickManager.Main mn = new CornerkickManager.Main(bContinuingTime: true, iTrainingsPerDay: 2, iTrainingsPerDayMax: 3, bWorldCup: true);
 
       //mn.setNewSeason();
 
